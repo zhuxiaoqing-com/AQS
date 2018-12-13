f0a42dac883e4027e706a06d72335803f355ca55
zhuxiaoqing <594012917@qq.com>
2018-12-13 11:34:47 +0800

    auto-commit

--- src/test/java/com/example/java8/Test02Stream.java
+++ src/test/java/com/example/java8/Test02Stream.java
@@ -3,7 +3,7 @@ package com.example.java8;
 import org.junit.Test;
 
 import java.util.*;
-import java.util.function.Function;
+import java.util.function.*;
 import java.util.stream.Collectors;
 
 public class Test02Stream {
@@ -16,5 +16,53 @@ public class Test02Stream {
 
     }
 
-//    public void stream01(int a, int b, )
+
+    @Test
+    public void test02() {
+        stream01(2, 4,
+                (x, y)-> x+y > 3,
+                (x,y) -> x+y,
+                x-> x>5);
+    }
+
+    /**
+     *
+     * @param oldParam 之前已经保存的数据
+     * @param addParam 当前的参数
+     * @param conditionFun 条件函数
+     * @param calculateFun 处理函数
+     * @param successFun 完成的条件值
+     */
+    public<O> void stream01(O oldParam, O addParam,
+                               BiPredicate<O, O> conditionFun,
+                            BinaryOperator<O> calculateFun,
+                               Predicate<O> successFun) {
+        // 是否已经完成
+        if(successFun.test(oldParam)) {
+            // 执行成功逻辑
+            System.out.println("成功 ：oldParam = " + oldParam);
+            return;
+        }
+        // 判断条件是否满足
+        if(conditionFun.test(oldParam, addParam)) {
+            // 进行处理
+            oldParam = calculateFun.apply(oldParam, addParam);
+            System.out.println("失败 ：oldParam = " + oldParam);
+        }
+
+        // 任务
+        // 1.将英雄升星到5星这个任务完成
+
+        //    1.英雄升星完毕后调用检测任务是否能完成
+
+        //      1.1 检测所有升星任务是否能完成任务
+                // 当前这个任务  进度   data 1
+                // 1.2
+                // if(mode==1)
+                {
+                this.data=0;
+                }
+
+
+    }
 }
