dbc8404fe5bb75f6bf06f01c0390ea8e1967d396
zhuxiaoqing <594012917@qq.com>
2018-12-27 11:31:08 +0800

    auto-commit

---
+++ src/test/java/com/example/java8/Test03Time.java
@@ -0,0 +1,16 @@
+package com.example.java8;
+
+import org.junit.Test;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+
+public class Test03Time {
+    @Test
+    public void test01() {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+        LocalTime parse = LocalTime.of(4,22);
+        System.out.println(parse);
+    }
+}
