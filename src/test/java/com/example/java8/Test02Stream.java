035d895c2689b3de28d7d259e8dae3f63c4e2cb7
zhuxiaoqing <594012917@qq.com>
2019-02-13 21:05:03 +0800

    auto-commit

--- src/test/java/com/example/java8/Test02Stream.java
+++ src/test/java/com/example/java8/Test02Stream.java
@@ -19,53 +19,39 @@ public class Test02Stream {
 
     @Test
     public void test02() {
-        stream01(2, 4,
-                (x, y)-> x+y > 3,
-                (x,y) -> x+y,
-                x-> x>5);
+        ArrayList<A> resources = new ArrayList<>();
+        resources.add(new A(1,3));
+        resources.add(new A(1,2));
+        resources.add(new A(2,3));
+        resources.add(new A(2,3));
+        Map<Integer, Integer> collect = resources.stream()
+                .collect(Collectors.groupingBy(A::getId, Collectors.summingInt(A::getCount)));
+        System.out.println(collect);
     }
 
-    /**
-     *
-     * @param oldParam 之前已经保存的数据
-     * @param addParam 当前的参数
-     * @param conditionFun 条件函数
-     * @param calculateFun 处理函数
-     * @param successFun 完成的条件值
-     */
-    public<O> void stream01(O oldParam, O addParam,
-                               BiPredicate<O, O> conditionFun,
-                            BinaryOperator<O> calculateFun,
-                               Predicate<O> successFun) {
-        // 是否已经完成
-        if(successFun.test(oldParam)) {
-            // 执行成功逻辑
-            System.out.println("成功 ：oldParam = " + oldParam);
-            return;
-        }
-        // 判断条件是否满足
-        if(conditionFun.test(oldParam, addParam)) {
-            // 进行处理
-            oldParam = calculateFun.apply(oldParam, addParam);
-            System.out.println("失败 ：oldParam = " + oldParam);
+    class A {
+        int id;
+        int count;
+
+        public A(int id, int count) {
+            this.id = id;
+            this.count = count;
         }
 
-        // 任务
-        // 1.将英雄升星到5星这个任务完成
+        public int getId() {
+            return id;
+        }
 
-        //    1.英雄升星完毕后调用检测任务是否能完成
+        public void setId(int id) {
+            this.id = id;
+        }
 
-        //      1.1 检测所有升星任务是否能完成任务
-                // 当前这个任务  进度   data 1
-                // 1.2
-                // if(mode==1)
-              /*  {
-                this.data=0;
-                }
-                */
-        /**
-         * @see Double
-          */
+        public int getCount() {
+            return count;
+        }
 
+        public void setCount(int count) {
+            this.count = count;
+        }
     }
 }
