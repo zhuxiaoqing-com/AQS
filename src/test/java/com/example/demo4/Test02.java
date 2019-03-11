7f88a39772910e4a946f40ac1e16c5e3f799a1a3
zhuxiaoqing <594012917@qq.com>
2019-03-11 18:08:11 +0800

    auto-commit

--- src/test/java/com/example/demo4/Test02.java
+++ src/test/java/com/example/demo4/Test02.java
@@ -1,6 +1,5 @@
 package com.example.demo4;
 
-import org.apache.ibatis.javassist.bytecode.analysis.Executor;
 import org.junit.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.ScannedGenericBeanDefinition;
@@ -16,7 +15,9 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.*;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 public class Test02 {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -54,31 +55,31 @@ public class Test02 {
     public void test01() {
         int s = Integer.MAX_VALUE;
         int s2 = 100;
-        int s3 = (int) (s*100L/100L);
+        int s3 = (int) (s * 100L / 100L);
         System.out.println(s3);
         System.out.println(s);
     }
 
     @Test
     public void test02() {
-       int id = 3032;
-        System.out.println(id%1000);
+        int id = 3032;
+        System.out.println(id % 1000);
     }
 
     @Test
     public void test03() {
         int id = 10000;
         int temp = 3;
-        System.out.println(3*10000/10000);
-        System.out.println(3*0.1/10000);
+        System.out.println(3 * 10000 / 10000);
+        System.out.println(3 * 0.1 / 10000);
     }
 
     @Test
     public void test04() {
         long id = 1_000_000000;
         int temp = 3;
-        System.out.println(id*id);
-        System.out.println(Math.toIntExact(id*id));
+        System.out.println(id * id);
+        System.out.println(Math.toIntExact(id * id));
     }
 
     @Test
@@ -94,8 +95,8 @@ public class Test02 {
         objects.add(1);
         System.out.println(objects);
 
-        Collections.sort(objects,(a,b)->{
-            return  a-b;
+        Collections.sort(objects, (a, b) -> {
+            return a - b;
         });
         System.out.println(objects);
     }
@@ -103,7 +104,7 @@ public class Test02 {
     @Test
     public void test06() {
         int hashcode = 3;
-        int mask = 4-1;
+        int mask = 4 - 1;
         /**
          * 1...000
          * 1...101
@@ -124,24 +125,24 @@ public class Test02 {
     @Test
     public void test08() {
         String s = "2d";
-        System.out.println(s == 2+"d");
+        System.out.println(s == 2 + "d");
     }
 
     public String join(int x) {
-        return x+"d";
+        return x + "d";
     }
 
     @Test
     public void test09() {
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(){
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>() {
             {
-                put(1,1);
-                put(2,2);
-                put(3,3);
-                put(4,4);
-                put(5,5);
-                put(6,6);
-                put(7,7);
+                put(1, 1);
+                put(2, 2);
+                put(3, 3);
+                put(4, 4);
+                put(5, 5);
+                put(6, 6);
+                put(7, 7);
             }
         };
         /*map.forEach((key, value)->{
@@ -149,7 +150,7 @@ public class Test02 {
             map.remove(key);
         });*/
         Collection<Integer> values = map.values();
-        for(Integer s:values) {
+        for (Integer s : values) {
             System.out.println(s);
             map.remove(s);
         }
@@ -158,7 +159,7 @@ public class Test02 {
     @Test
     public void test10() {
         List<Integer> integers = Arrays.asList(2, 3, 4, 1, 3, 5);
-        Collections.sort(integers,(a,b)-> b-a);
+        Collections.sort(integers, (a, b) -> b - a);
         System.out.println(integers);
     }
 
@@ -166,10 +167,11 @@ public class Test02 {
     public void test11() {
         System.out.println(exception());
     }
+
     public int exception() {
         int i = 10;
         try {
-            int a= 1/0;
+            int a = 1 / 0;
             return 1;
         } catch (Exception e) {
             return i;
@@ -197,32 +199,79 @@ public class Test02 {
     public void test14() {
         long l = 2L * 60 * 60 * 1000;
         System.out.println(l);
-        System.out.println(l/4);
-        System.out.println(l/(1000*60));
+        System.out.println(l / 4);
+        System.out.println(l / (1000 * 60));
     }
 
     @Test
     public void test15() {
-        System.out.println(0%2);
-        System.out.println(2%0);
+        System.out.println(0 % 2);
+        System.out.println(2 % 0);
         System.out.println(new Date(1551517260000L));
     }
 
     @Test
-    public void test16() {
+    public void test16() { //开朗的帕吉特
         int max = 13234;
         int i = 13;
-        System.out.println(i*10000/13234);
-        System.out.println(0%2);
+        System.out.println(i * 10000 / 13234);
+        System.out.println(0 % 2);
         System.out.println(new Date(1551517260000L));
     }
+
+    @Test
+    public void test17() { //开朗的帕吉特
+        int rank1 = 9400;
+        int[] rank = new int[]{10000, 9001};
+        int[] level = new int[]{10, 1};
+
+        int total1 = rank[0] - rank[1] + 1;
+        int total2 = level[0] - level[1] + 1;
+
+        int i1 = rank[1] - rank1;
+
+        double i = total1 * 1.0 / total2;
+
+        System.out.println((10000 - rank1) / i);
+    }
+
+    /**
+     * 1、找到排名所在的区间
+     * 2、排名 和 等级的比例
+     * 3、根据比例换算等级下标
+     */
+
+    @Test
+    public void fun01() {
+        int rank1 = 9400;
+        int[] rank = new int[]{10000, 9001};
+        int[] level = new int[]{4, 10};
+
+        double k = (level[1] - level[0]) * 1.0 / (rank[0] - rank[1]);
+
+        int y = (int) (level[0] + k * (rank[0] - rank1));
+        System.out.println(y);
+        int i = countRangeMappingRange(rank, rank1, level);
+        System.out.println(i);
+    }
+
+    private int countRangeMappingRange(int[] knowRange, int know, int[] unknownRange) {
+        // 获取 know 在 knowRange 的下标 know - knowRange[0] 代表着 know 与 knowRange[0] 相差多少
+        int knowIndex = Math.abs(know - knowRange[0]);
+
+        // 因为 knowRange[0] - knowRange[1] 只是 得出 knowRange[1] 的下标 为 (length-1) 所以还需要 + 1
+        int length = Math.abs(knowRange[0] - knowRange[1]) + 1;
+        int unLength = Math.abs(unknownRange[0] - unknownRange[1]) + 1;
+        // 得出 一份 length 对应几分 unLength
+        double percent = (unLength + 0.0) / length;
+
+        // 得出 knowIndex 在 knowRange 下标 对应 unknownRange 所在的下标
+        int unKnowIndex = (int) (knowIndex * percent);
+
+        // 得出 unKnowIndex 下标所在 unknownRange 的值
+        return unknownRange[0] > unknownRange[1] ? unknownRange[0] - unKnowIndex : unknownRange[0] + unKnowIndex;
+    }
 }
-/**
- *
- */
-
-
-
 
 
 
