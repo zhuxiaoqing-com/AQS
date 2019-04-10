9ea327d74c21d0c1fbae24bfd52e803fccea18b1
zhuxiaoqing <594012917@qq.com>
2019-04-10 11:32:57 +0800

    auto-commit

---
+++ src/test/java/com/example/demo4/Main.java
@@ -0,0 +1,12 @@
+package com.example.demo4;
+
+public class Main {
+
+    public Main() {
+        System.out.println("constructor");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("main");
+    }
+}
