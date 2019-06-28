7e437c6239e2bbd95f1a5d02a8e942333b97e885
zhuxiaoqing <594012917@qq.com>
2019-06-28 18:08:35 +0800

    auto-commit

--- src/test/java/com/example/demo4/Test04.java
+++ src/test/java/com/example/demo4/Test04.java
@@ -1,33 +1,34 @@
 package com.example.demo4;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Test04 {
 
     @Test
     public void test01() {
         long start = System.currentTimeMillis();
-        for(int i =0; i<Integer.MAX_VALUE;i++) {
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
             System.currentTimeMillis();
         }
 
         long end = System.currentTimeMillis();
 
         long start2 = System.currentTimeMillis();
-        for(int i =0; i<Integer.MAX_VALUE;i++) {
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
             System.currentTimeMillis();
         }
 
         long end2 = System.currentTimeMillis();
         System.out.println((end2 - start2));
     }
+
     @Test
     public void test02() {
-        String s ="\\|";
-        String s1 ="|";
+        String s = "\\|";
+        String s1 = "|";
         String aa = "1|3|2|3|1";
         System.out.println(Arrays.toString(aa.split(s)));
         System.out.println(Arrays.toString(aa.split(s1)));
@@ -35,13 +36,13 @@ public class Test04 {
 
     @Test
     public void test03() {
-        String s ="0|203015|1&0|103|10&0|42001|10&0|42100|5&0|42123|5&0|101|150000";
+        String s = "0|203015|1&0|103|10&0|42001|10&0|42100|5&0|42123|5&0|101|150000";
         System.out.println(s.length());
     }
 
     @Test
     public void test04() {
-        String s ="383490927757018";
+        String s = "383490927757018";
         System.out.println(s.length());
     }
 
@@ -49,7 +50,7 @@ public class Test04 {
     public void test05() {
         ArrayList<Integer> list = new ArrayList<>();
 
-        for(int i = 0; i<= 10000; i++) {
+        for (int i = 0; i <= 10000; i++) {
             list.add(i);
         }
 
@@ -60,6 +61,32 @@ public class Test04 {
 
     @Test
     public void test06() {
-        System.out.println(20000/30);
+        long a = 100_000_000_000_000L;
+        long b = 123456789;
+        StringBuilder builder = new StringBuilder(200);
+        HashMap<Long, Long> hashMap = new LinkedHashMap<>();
+
+        for (int i = 0; i <= 600; i++) {
+            hashMap.put(a + i, b);
+        }
+        Set<Map.Entry<Long, Long>> entries = hashMap.entrySet();
+        int rank = 1;
+        for (Map.Entry<Long, Long> entry : entries) {
+
+            builder.append(entry.getKey());
+            builder.append("|");
+            builder.append(rank);
+            builder.append("|");
+            builder.append(entry.getValue());
+            builder.append("&");
+        }
+        System.out.println(builder.subSequence(0, builder.length() - 1));
     }
+
+    @Test
+    public void test07(){
+        StringBuilder builder = new StringBuilder("0123456");
+        System.out.println(builder.delete(0,1));
+    }
+
 }
