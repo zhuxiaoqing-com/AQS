6e176fcaba6687bb08c3e2d86c7129cbce35cf58
zhuxiaoqing <594012917@qq.com>
2019-11-27 21:05:38 +0800

    auto-commit

--- src/test/java/com/example/demo4/Test07.java
+++ src/test/java/com/example/demo4/Test07.java
@@ -153,19 +153,16 @@ public class Test07 {
 
     @Test
     public void test08() {
-        char a = 11;
-       int i = 0;
-       switch (i){
-           default:
-               System.out.println("default......");
-               break;
-           case 2:
-               System.out.println(2);
-           case  1:
-               System.out.println(1);
-       }
+        ArrayList<List<Integer>> list = new ArrayList<>();
+        a(list,Integer.class);
+    }
+
+    private<T> void a(List<List<T>> list, Class<T> c){
+        HashMap<Object, Object> hashMap = new HashMap<>();
 
     }
+
+
 }
 
 
