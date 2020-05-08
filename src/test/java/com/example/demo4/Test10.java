77683958602ea71b7e8307c498e942cad44a2a92
10heros <594012917@qq.com>
2020-05-08 17:02:11 +0800

    auto-commit

--- src/test/java/com/example/demo4/Test10.java
+++ src/test/java/com/example/demo4/Test10.java
@@ -5,10 +5,7 @@ import org.junit.Test;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Test10 {
 	@Test
@@ -202,19 +199,67 @@ public class Test10 {
 	@Test
 	public void test14() {
 		long start01 = System.nanoTime();
-		for(int i = 0; i<=1000_000_000; i++){
-			if(1>2){
+		for (int i = 0; i <= 1000_000_000; i++) {
+			if (1 > 2) {
 			}
 		}
 
 		System.out.println(System.nanoTime() - start01);
 
 		long start02 = System.nanoTime();
-		for(int i = 0; i<=1000_000_000; i++){
-			if(1>=2){
+		for (int i = 0; i <= 1000_000_000; i++) {
+			if (1 >= 2) {
 			}
 		}
 		System.out.println(System.nanoTime() - start02);
+	}
+
+	@Test
+	public void test15() {
+		Map<Integer, Integer> hashMap = new HashMap<>();
+		Map<Integer, Integer> treeMap = new TreeMap<>();
+
+		long start = System.nanoTime();
+		for (int i = 0; i < 500; i++) {
+			hashMap.put(i, i);
+		}
+		System.out.println(System.nanoTime() - start);
+
+		start = System.nanoTime();
+		for (int i = 0; i < 500; i++) {
+			treeMap.put(i, i);
+		}
+		System.out.println(System.nanoTime() - start);
+
+		start = System.nanoTime();
+		for (int i = 0; i < 500; i++) {
+			hashMap.get(3);
+		}
+		System.out.println(System.nanoTime() - start);
+
+		start = System.nanoTime();
+		for (int i = 0; i < 500; i++) {
+			treeMap.get(250);
+		}
+		System.out.println(System.nanoTime() - start);
+
+	}
+
+	@Test
+	public void test16() {
+		Map<Integer, Integer> hashMap1 = new HashMap<Integer, Integer>(){
+			{
+				put(1,1);
+			}
+		};
+		Map<Integer, Integer> hashMap2 = new HashMap<Integer, Integer>(){
+			{
+				put(1,1);
+			}
+		};
+
+
+		System.out.println(hashMap1.equals(hashMap2));
 	}
 }
 
