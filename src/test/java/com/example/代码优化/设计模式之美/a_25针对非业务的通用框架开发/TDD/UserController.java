b4d4df0dd9deea4bb62f82619eea29723573b1c0
10heros <594012917@qq.com>
2020-05-22 18:30:48 +0800

    auto-commit

--- src/test/java/com/example/代码优化/设计模式之美/a_25针对非业务的通用框架开发/TDD/UserController.java
+++ src/test/java/com/example/代码优化/设计模式之美/a_25针对非业务的通用框架开发/TDD/UserController.java
@@ -1,16 +1,28 @@
 package com.example.代码优化.设计模式之美.a_25针对非业务的通用框架开发.TDD;
 
+import com.example.代码优化.设计模式之美.a_25针对非业务的通用框架开发.prototype.Metrics;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * @Auther: zhuxiaoqing
  * @Date: 2020/5/21 20:53
  * @Description:
  */
 public class UserController {
-	public void register(Object object){
-		//...
+	private Metrics metrics = new Metrics();
+
+	public UserController() {
+		this.metrics.startRepeatedReport(60, TimeUnit.SECONDS);
 	}
 
-	public Object login(String telephone, String password){
+	public void register(Object object) {
+		//...
+		long startTimestamp = System.currentTimeMillis();
+		metrics.recordTimestamp("register", startTimestamp);
+	}
+
+	public Object login(String telephone, String password) {
 		//...
 		return null;
 	}
--- src/test/java/com/example/代码优化/设计模式之美/a_25针对非业务的通用框架开发/prototype/Metrics.java
+++ src/test/java/com/example/代码优化/设计模式之美/a_25针对非业务的通用框架开发/prototype/Metrics.java
@@ -1,8 +1,14 @@
 package com.example.代码优化.设计模式之美.a_25针对非业务的通用框架开发.prototype;
 
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Auther: zhuxiaoqing
@@ -17,10 +23,57 @@ public class Metrics {
 	 */
 
 
-
 	// Map 的 Key 是接口名称， value 对应接口请求的响应时间或时间戳;
 	private Map<String, List<Double>> responseTimes = new HashMap<>();
 
 	private Map<String, List<Double>> timestamps = new HashMap<>();
 
+	private ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+	public void recordResponseTime(String apiName, double responseTime) {
+		responseTimes.computeIfAbsent(apiName, a -> new ArrayList<>()).add(responseTime);
+	}
+
+	public void recordTimestamp(String apiName, double timestamp) {
+		responseTimes.computeIfAbsent(apiName, a -> new ArrayList<>()).add(timestamp);
+	}
+
+	public void startRepeatedReport(long period, TimeUnit unit) {
+		executor.scheduleWithFixedDelay(() -> {
+			Gson gson = new Gson();
+			Map<String, Map<String, Double>> stats = new HashMap<>();
+			for (Map.Entry<String, List<Double>> entry: responseTimes.entrySet()){
+				String apiName = entry.getKey();
+				List<Double> apiRespTimes = entry.getValue();
+				stats.putIfAbsent(apiName, new HashMap<>());
+				stats.get(apiName).put("max", max(apiRespTimes));
+				stats.get(apiName).put("avg", avg(apiRespTimes));
+			}
+			for (Map.Entry<String, List<Double>> entry: timestamps.entrySet()){
+				String apiName = entry.getKey();
+				List<Double> apiTimestamps = entry.getValue();
+				stats.putIfAbsent(apiName, new HashMap<>());
+				stats.get(apiName).put("count", (double)apiTimestamps.size());
+			}
+			System.out.println(gson.toJson(stats));
+		}, 0, period, unit);
+	}
+
+
+	private Double avg(List<Double> apiRespTimes) {
+		return -1d;
+	}
+
+	private Double max(List<Double> apiRespTimes) {
+		return -1d;
+	}
+
 }
+
+
+
+
+
+
+
+
