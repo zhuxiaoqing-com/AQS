242d88a426552c81e8b0f708ccb47b5121da828c
10heros <594012917@qq.com>
2020-05-23 18:33:32 +0800

    auto-commit

--- src/test/java/com/example/demo4/Test11.java
+++ src/test/java/com/example/demo4/Test11.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Test11 {
 	@Test
@@ -17,6 +19,36 @@ public class Test11 {
 		}
 		long end = System.nanoTime();
 		System.out.println((end - start) / 1000);
+	}
+
+	@Test
+	public void test02() {
+		int x = 3;
+		int k = 1;
+		System.out.println(x/(1<<k));
+
+		System.out.println(x>>(k));
+		System.out.println((x+(1<<k)-1)>>k);
+
+		System.out.println();
+
+		ArrayList<String> list = new ArrayList<>();
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		list.stream().map(member -> {
+			if (member.startsWith("\"")) {
+				member = member.substring(1, member.length() - 1);
+			}
+			return Integer.parseInt(member);
+		}).collect(Collectors.toList());
+
+	}
+
+	@Test
+	public void test03() {
+		String s = "a231231s";
+		System.out.println(s.substring(1,s.length()-1));
 	}
 
 
