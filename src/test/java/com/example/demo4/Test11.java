cfa8dfa4ceffd0616a6948e19597cd5ff417d7e0
10heros <594012917@qq.com>
2020-07-25 23:26:52 +0800

    auto-commit

--- src/test/java/com/example/demo4/Test11.java
+++ src/test/java/com/example/demo4/Test11.java
@@ -243,6 +243,12 @@ public class Test11 {
 		System.out.println(s.lastIndexOf("/"));
 	}
 
+	@Test
+	public void test21() {
+	 String a = "// {{{ XMLGEN_IMPORT_BEGIN";
+		public static final String DO_NOT_EDIT_THIS_START = "{{{ XMLGEN_DEFINE_BEGIN";
+	}
+
 
 
 }
