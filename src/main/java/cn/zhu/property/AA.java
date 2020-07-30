addd4e04f67e80e95439e44172a594541c5c83da
10heros <594012917@qq.com>
2020-07-30 21:59:22 +0800

    auto-commit

---
+++ src/main/java/cn/zhu/property/AA.java
@@ -0,0 +1,9 @@
+package cn.zhu.property;
+
+/**
+ * @Auther: zhuxiaoqing
+ * @Date: 2020/7/30 17:49
+ * @Description:
+ */
+public class AA {
+}
---
+++ src/main/java/cn/zhu/property/PropertyDemo1.java
@@ -0,0 +1,27 @@
+package cn.zhu.property;
+
+/**
+ * @Auther: zhuxiaoqing
+ * @Date: 2020/7/30 17:47
+ * @Description:
+ */
+public class PropertyDemo1 {
+	private int age;
+	private String name;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
