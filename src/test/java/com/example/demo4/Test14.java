db2c6d17bebdc5f7493f0b1c0aa52f3e117c5733
10heros <594012917@qq.com>
2020-09-14 21:07:24 +0800

    auto-commit

--- src/test/java/com/example/demo4/Test14.java
+++ src/test/java/com/example/demo4/Test14.java
@@ -1,5 +1,6 @@
 package com.example.demo4;
 
+import io.swagger.models.auth.In;
 import org.junit.Test;
 
 /**
@@ -25,6 +26,28 @@ public class Test14 {
 			sum2++;
 		}
 		System.out.println(System.currentTimeMillis() - start);
+	}
+
+
+	@Test
+	public void test01() {
+		int value = 555553135;
+		System.out.println(Integer.toBinaryString(value));
+		while ((value & -128) != 0) {
+			int s = (int) value & 127 | 128;
+			System.out.println(Integer.toBinaryString(s));
+			value >>>= 7;
+		}
+
+	}
+
+	@Test
+	public void test02() {
+		int value = 555553135;
+		int s = (int) value & 127 | 128;
+		System.out.println(Integer.toBinaryString(value&127));
+		System.out.println(Integer.toBinaryString(value&127|128));
+
 	}
 }
 
